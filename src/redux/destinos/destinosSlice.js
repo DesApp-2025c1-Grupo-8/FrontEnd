12aff11738fcb0d9b2b5c9963e1e8fe469c0e88b
AleHutmacher <hutmacheralejo@gmail.com>
2025-05-15 07:25:29 -0300

    changes(redux):
    - some modifications were done to the destinations redux

--- src/redux/destinos/destinosSlice.js
+++ src/redux/destinos/destinosSlice.js
@@ -1,81 +1,74 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   lista: [
     {
       id: "0001",
       Pais: "Argentina",
-      CodPais: "ARG",
       "Provincia/Estado": "Buenos Aires",
-      CodPE: "001",
-      "Distrito/Municipio": "Hurlingham",
-      CodDist: "0135",
+      Municipio: "Hurlingham",
+      Localidad: "Hurlingham",
+      CodigoPostal: "0135",
       Calle: "Avenida Gobernador Vergara",
       Altura: "1234",
     },
     {
       id: "0002",
       Pais: "Argentina",
-      CodPais: "ARG",
       "Provincia/Estado": "Buenos Aires",
-      CodPE: "001",
-      "Distrito/Municipio": "Hurlingham",
-      CodDist: "0135",
+      Municipio: "Hurlingham",
+      Localidad: "Hurlingham",
+      CodigoPostal: "0135",
       Calle: "Avenida Roca",
       Altura: "2531",
     },
     {
       id: "0003",
       Pais: "Argentina",
-      CodPais: "ARG",
       "Provincia/Estado": "Buenos Aires",
-      CodPE: "001",
-      "Distrito/Municipio": "Hurlingham",
-      CodDist: "0135",
+      Municipio: "Hurlingham",
+      Localidad: "Hurlingham",
+      CodigoPostal: "0135",
       Calle: "Avenida Gobernador Vergara",
       Altura: "7534",
     },
     {
       id: "0004",
       Pais: "Argentina",
-      CodPais: "ARG",
       "Provincia/Estado": "Buenos Aires",
-      CodPE: "001",
-      "Distrito/Municipio": "Hurlingham",
-      CodDist: "0135",
+      Municipio: "Hurlingham",
+      Localidad: "Hurlingham",
+      CodigoPostal: "0135",
       Calle: "Avenida Jauretche",
       Altura: "456",
     },
     {
       id: "0005",
       Pais: "Argentina",
-      CodPais: "ARG",
       "Provincia/Estado": "Buenos Aires",
-      CodPE: "001",
-      "Distrito/Municipio": "Hurlingham",
-      CodDist: "0135",
+      Municipio: "Hurlingham",
+      Localidad: "Hurlingham",
+      CodigoPostal: "0135",
       Calle: "Bustamante",
       Altura: "1696",
     },
     {
       id: "0006",
       Pais: "Argentina",
-      CodPais: "ARG",
       "Provincia/Estado": "Buenos Aires",
-      CodPE: "001",
-      "Distrito/Municipio": "Hurlingham",
-      CodDist: "0135",
+      Municipio: "Hurlingham",
+      Localidad: "Hurlingham",
+      CodigoPostal: "0135",
       Calle: "Argerich",
       Altura: "976",
     },
     {
       id: "0007",
       Pais: "Argentina",
-      CodPais: "ARG",
       "Provincia/Estado": "Buenos Aires",
-      CodPE: "001",
-      "Distrito/Municipio": "Hurlingham",
-      CodDist: "0135",
+      Municipio: "Hurlingham",
+      Localidad: "Hurlingham",
+      CodigoPostal: "0135",
       Calle: "Ricchieri",
       Altura: "1616",
     },
@@ -84,14 +77,14 @@ const initialState = {
 };
 
 const destinosSlice = createSlice({
-  name: 'destinos',
+  name: "destinos",
   initialState,
   reducers: {
     agregarDestino(state, action) {
       state.lista.push(action.payload);
     },
     eliminarDestino(state, action) {
-      state.lista = state.lista.filter(d => d.id !== action.payload);
+      state.lista = state.lista.filter((d) => d.id !== action.payload);
     },
     seleccionarDestino(state, action) {
       state.seleccionado = action.payload;
